be2a4697b8cccbef46072368548e2f423296cfb7
Tomaser <mrklimatomas@gmail.com>
2016-05-21 23:21:43 +0200

    added "search", which currently just dumps a json of selected query.

--- Python_test1.py
+++ Python_test1.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import urllib2
 
+# key = "dc6zaTOxFJmzC"
+
+
+# http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 app = Flask(__name__)
+key = "dc6zaTOxFJmzC"
 
 
 @app.route('/asd')
@@ -8,9 +14,21 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/test')
-def something():
-    return 'lalala falala'
+@app.route('/')
+def search():
+    query = raw_input("Insert search query: ")
+    real_query = ""
+    for i in query:
+        if i == " ":
+            real_query += "+"
+        else:
+            real_query += i
+
+    response = urllib2.urlopen("http://api.giphy.com/v1/gifs/search?q=" + real_query + "&api_key=" + key)
+    result = response.read()
+    print result
+
+    return "Search results for: " + query + result
 
 
 if __name__ == '__main__':
