ce13737cd6fcdea87881987786da84049679fd6f
Tomas <mrklimatomas@gmail.com>
2016-05-22 23:21:27 +0200

    preliminary version of joining gifs with sound

--- Python_test1.py
+++ Python_test1.py
@@ -1,45 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 import gunicorn
-import urllib2
-import json
+import requests
+from json import dumps
 
-# key = "dc6zaTOxFJmzC"
+# import grequests
 
-
-# http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 app = Flask(__name__)
 key = "dc6zaTOxFJmzC"
-json_dict = {}
-
-
-@app.route('/asd')
-def hello_world():
-    return 'Hello World!'
 
 
 @app.route('/')
 def search():
-    query = "cute tortoise"
-    real_query = ""
-    for i in query:
-        if i == " ":
-            real_query += "+"
-        else:
-            real_query += i
+    query = "kittens".replace(" ", "+")
+    gif_array = []
+    gif_request = requests.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + key)
+    gif_data = gif_request.json()
 
-    json_object = urllib2.urlopen("http://api.giphy.com/v1/gifs/search?q=" + real_query + "&api_key=" + key)
-    data = json.load(json_object)
+    for item in gif_data["data"]:
+        spotify_request = requests.get("https://api.spotify.com/v1/search?type=track&limit=1&q=" + query)
+        gif_music = spotify_request.json()
 
-    #
-    # for item in data["data"]:
-    #     if not "url" in json_dict:
-    #         json_dict["url"] = [item["url"]]
-    #     else:
-    #         json_dict["url"].append(item["url"])
+        gif_array.append(
+            {"url": item["images"]["original"]["mp4"], "song": gif_music["tracks"]["items"][0]["preview_url"]})
 
-
-   # return json_dict
+    return dumps(gif_array)
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
