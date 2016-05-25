af61f960106526b3765b99f633d6fca06b959046
Tomas <mrklimatomas@gmail.com>
2016-05-25 20:54:11 +0200

    Test_heroku

--- Python_test1.py
+++ Python_test1.py
@@ -1,27 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask
+from json import dumps
 import gunicorn
 import requests
-from json import dumps
-
-# import grequests
+import grequests
 
 app = Flask(__name__)
-key = "dc6zaTOxFJmzC"
+giphy_key = "dc6zaTOxFJmzC"
 
 
 @app.route('/')
 def search():
-    query = "kittens".replace(" ", "+")
+    query = "little turtles".replace(" ", "+")
     gif_array = []
-    gif_request = requests.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + key)
+
+    gif_request = requests.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=" + giphy_key)
     gif_data = gif_request.json()
+    req_set = []
 
     for item in gif_data["data"]:
-        spotify_request = requests.get("https://api.spotify.com/v1/search?type=track&limit=1&q=" + query)
-        gif_music = spotify_request.json()
+        spotify_request = grequests.get("https://api.spotify.com/v1/search?type=track&limit=1&q=" + query)
+        req_set.append(spotify_request)
 
+    res_ary = grequests.map(req_set)
+
+    greq_count = 0
+
+    for item in gif_data["data"]:
+        gif_music = res_ary[greq_count].json()
         gif_array.append(
             {"url": item["images"]["original"]["mp4"], "song": gif_music["tracks"]["items"][0]["preview_url"]})
+
+        greq_count += 1
 
     return dumps(gif_array)
 
