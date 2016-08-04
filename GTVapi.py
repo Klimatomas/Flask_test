29c57fd711c247618eb9da278843965bb990d72b
tomaser <mrklimatomas@gmail.com>
2016-08-04 11:35:14 +0200

    Updates to url Improved assigning function

--- GTVapi.py
+++ GTVapi.py
@@ -3,9 +3,10 @@ import requests
 from json import dumps
 from flask import Flask
 
-
 app = Flask(__name__)
 giphy_key = "api_key=dc6zaTOxFJmzC"
+sptf_url = "https://api.spotify.com/v1/search?type=track&limit=1&q="
+trnd_url = "http://api.giphy.com/v1/gifs/trending?"
 
 
 def slug_interpreter(slug, ident):
@@ -25,30 +26,23 @@ def search():
     gif_array = []
     req_set = []
     ary = []
-    gif_request = requests.get("http://api.giphy.com/v1/gifs/trending?" + giphy_key)
-    gif_data = gif_request.json()
+    gif_data = requests.get(trnd_url + giphy_key).json()
 
     for item in gif_data["data"]:
         query = slug_interpreter(str(item["slug"]), str(item["id"]))
         if query:
             ary.append({"slug": item["slug"], "url": item["images"]["original"]["mp4"]})
-            spotify_request = grequests.get(
-                "https://api.spotify.com/v1/search?type=track&limit=1&q=" + query)
-            req_set.append(spotify_request)
+            req_set.append(grequests.get(sptf_url + query))
 
     res_ary = grequests.map(req_set)
 
-    greq_count = 0
-
-    for item in ary:
-        gif_music = res_ary[
-            greq_count].json() 
+    for i, item in enumerate(ary):
+        gif_music = res_ary[i].json()
         if gif_music["tracks"]["total"] == 0:
             continue
         else:
             gif_array.append(
                 {"url": item["url"], "song": gif_music["tracks"]["items"][0]["preview_url"]})
-        greq_count += 1
 
     return dumps(gif_array)
 
